f6fccd790e0bbad9b64fb94c8a01551b756b1ba1
5tirner <thatfor1337@gmail.com>
2024-04-25 16:57:12 +0100

    zasabri

--- numbers.py
+++ numbers.py
@@ -1,7 +1,14 @@
+import random
+
 var1 = int(1)
 var2 = float(1)
 var3 = complex(3)
 
 print(var1,'->', type(var1))
 print(var2,'->', type(var2))
+print(var3,'->', type(var3))
+print('Generate A Random Number:', random.randrange(1, 10))
+var3 = float("1")
+print(var3,'->', type(var3))
+var3 = str(1.0j)
 print(var3,'->', type(var3))
