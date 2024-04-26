4afcf0291ea4214d466e45c4983624a8e02c94aa
5tirner <thatfor1337@gmail.com>
2024-04-26 17:35:03 +0100

    zasabri

--- lists.py
+++ lists.py
@@ -11,5 +11,17 @@ print(createListWithConstructor[-2])
 print(createListWithConstructor[1:3])
 if 1 in createListWithConstructor:
     print("One Appear In The List")
-createListWithConstructor[1:3] = ["10", "11"]
+createListWithConstructor[1:3] = ["10", 11]
+print(createListWithConstructor)
+createListWithConstructor.insert(0, "GOOD")
+print(createListWithConstructor)
+createListWithConstructor.append("Bad")
+print(createListWithConstructor)
+createListWithConstructor.extend(aList)
+print(createListWithConstructor)
+createListWithConstructor.remove(1) #Delete By Content
+print(createListWithConstructor)
+createListWithConstructor.pop(-1) #Delete By Index
+print(createListWithConstructor)
+del createListWithConstructor[-1] #Delete By Index
 print(createListWithConstructor)
