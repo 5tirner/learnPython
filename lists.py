60f0c6ee33823e36d5982f0f130b1d2020efd2d2
5tirner <thatfor1337@gmail.com>
2024-04-29 11:03:21 +0100

    zasabri

--- lists.py
+++ lists.py
@@ -25,3 +25,36 @@ createListWithConstructor.pop(-1) #Delete By Index
 print(createListWithConstructor)
 del createListWithConstructor[-1] #Delete By Index
 print(createListWithConstructor)
+createListWithConstructor.clear()
+print(createListWithConstructor)
+createListWithConstructor.insert(0, "Hi")
+print(createListWithConstructor)
+createListWithConstructor = list((1, 2, 3, 4, 5, 6))
+# for n in createListWithConstructor :
+#     print(n)
+# for i in range(len(createListWithConstructor)) :
+#     print(createListWithConstructor[i])
+
+# i = 0
+# while i < len(createListWithConstructor) :
+#     print(createListWithConstructor[i])
+#     i += 1
+# [print(x) for x in createListWithConstructor]
+lista = [fill for fill in createListWithConstructor if fill % 2 == 0]
+print(lista)
+lista2 = [364, 4358, 438, 3478, 356784, 453, 4, 3264, 3475, 435, 46, 34]
+# lista2.sort(reverse = True)
+print(lista2)
+def compareStr(a, b) :
+    i = j = 0
+    while i < len(a) or j < len(b) :
+        if (a[i] != b[j]) :
+            return ord(a[i]) - ord(b[j])
+            # print(ord(a[i]), ord(b[j]))
+        i += 1
+        j += 1
+    return 0
+a, b = "ABC", "ABD"
+print(compareStr(a, b))
+lista3 = lista2.copy()
+print(lista3[2:5])
