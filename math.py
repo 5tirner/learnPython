993b4e69613799253f061ccaf65b1fe63a0cc203
5tirner <thatfor1337@gmail.com>
2024-04-30 10:43:15 +0100

    zasabri

---
+++ math.py
@@ -0,0 +1,12 @@
+import math
+lista = [213,234,343,5435,345,-214]
+print(min(lista))
+print(lista)
+lista.append(4365784)
+print(lista)
+lista.insert(2, 78)
+print(lista)
+print(max(lista))
+print(abs(-1))
+print(pow(4,4))
+print(math.sqrt(9))
