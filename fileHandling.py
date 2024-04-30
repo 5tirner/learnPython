f93db19e58076aedd83a038df435bbad8b1303e8
5tirner <thatfor1337@gmail.com>
2024-04-30 18:47:17 +0100

    zasabri

--- fileHandling.py
+++ fileHandling.py
@@ -1,6 +1,7 @@
+import os
 print("Read From A File:\n")
 try:
-    file = open("getStarted.py", "r")
+    file = open("file.txt", "r")
     try:
         #save = file.read() read all
        # save1 = file.read(5) read 5 bytes
@@ -35,3 +36,7 @@ finally:
     file.close()
     file1.close()
     file2.close()
+    if os.path.exists("file.txt"):
+        os.remove("file.txt")
+    file = open("file.txt1", "x")
+    file.close()
