b4446b0552c857674252fbdbc404ca7c7f695754
5tirner <thatfor1337@gmail.com>
2024-04-30 10:36:40 +0100

    zasabri

---
+++ dates.py
@@ -0,0 +1,14 @@
+import datetime as DATE
+
+print (DATE.datetime.now())
+print (DATE.datetime.now().year)
+print (DATE.datetime.now().month)
+print (DATE.datetime.now().day)
+print (DATE.datetime.now().hour)
+print (DATE.datetime.now().minute)
+print (DATE.datetime.now().second)
+print (DATE.datetime.now().microsecond)
+print (DATE.datetime(1, 1, 1))
+print (DATE.datetime.now().strftime("%A"))
+print (DATE.datetime.now().strftime("%a"))
+print (DATE.datetime.now().strftime("%w"))
