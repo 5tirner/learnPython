4199cb7a365bc8d1f10c413744faf68ab2e3e312
5tirner <thatfor1337@gmail.com>
2024-05-01 14:58:14 +0100

    zasabri

--- machineLearning.py/DATADistribuition.py
+++ machineLearning.py/DATADistribuition.py
@@ -1,5 +1,29 @@
 import numpy
-import matplotlib.puplot as window
+import matplotlib.pyplot as window
 import scipy.stats as st
 import math
 
+numbers = numpy.random.uniform(5, size=(250))
+y = numpy.random.randint(100, size=(250))
+front = numpy.random.uniform(0.1, 0.3, 250)
+colors = numpy.random.randint(250, size=(250))
+sizes = numpy.random.randint(1000, size=(250))
+index = 1
+for n in numbers:
+    print(f"Number {index}: {n}")
+    index+=1
+window.title("Pie Data Distribution", loc="left")
+window.pie(numbers, startangle=0, explode=front, shadow=True)
+window.show()
+window.title("Hist Data Distribution", loc="left")
+window.hist(numbers)
+window.show()
+window.title("Bar Data Distribution", loc="left")
+window.bar(numbers, numbers, width=0.3)
+window.show()
+window.title("Plot Data Distribution", loc="left")
+window.plot(numbers, y, c='k')
+window.show()
+window.title("Scatter Data Distribution", loc="left")
+window.scatter(numbers, y, c=colors, s=sizes, alpha=0.2, cmap='nipy_spectral')
+window.show()
